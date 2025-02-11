3a4686f37b435577b6be206225d6db90c7155cab
Favour A. Uzochukwu <uzochukwu.favour310@gmail.com>
2025-02-11 08:25:02 +0100

    Update:corrected the icon that displays for the toggle feature

--- scripts/popup.js
+++ scripts/popup.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     const enabled = result.enabled !== undefined ? result.enabled : false;
     button.innerHTML = enabled
     // Implementation of the toggle button
-      ? ` Off <i class="bi bi-toggle-off" style="font-size: 2em"></i>On`
-      : ` Off <i class="bi bi-toggle-on" style="font-size: 2em"></i>On`;
+      ? ` Off <i class="bi bi-toggle-on" style="font-size: 2em"></i>On`
+      : ` Off <i class="bi bi-toggle-off" style="font-size: 2em"></i>On`;
   };
 
   // Handles the togge effect of the button
